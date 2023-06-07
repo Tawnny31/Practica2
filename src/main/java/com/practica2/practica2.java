fbbc54dc70bdedb55af84d8e34726d67468fd84c
Tawnny31 <105888154+Tawnny31@users.noreply.github.com>
2023-06-06 19:17:37 -0600

    Practica2-primer commit

---
+++ src/main/java/com/practica2/practica2.java
@@ -0,0 +1,13 @@
+package com.practica2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class practica2 {
+
+	public static void main(String[] args) {
+		SpringApplication.run(practica2.class, args);
+	}
+
+}
